618b6db8ad2075b235c0365936923769f4620eb7
onlystar <69192995+15804013102@users.noreply.github.com>
2020-08-19 16:13:17 +0800

    Update HelloWorld.java
    打我呀

--- Project02/src/HelloWorld.java
+++ Project02/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		   System.out.println("HelloWorld");
+		   System.out.println("改你的东西 你打我呀");
 	}
 
 }
