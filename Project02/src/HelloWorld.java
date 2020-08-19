e3b1e4c575a2e33e9e68053d57dceddb57f273ca
onlystar <69192995+15804013102@users.noreply.github.com>
2020-08-19 16:51:02 +0800

    Update HelloWorld.java
    1

--- Project02/src/HelloWorld.java
+++ Project02/src/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
 		   System.out.println("小逼崽子改我东西 你给我等着 看我打死你");
 		   System.out.println("有种就来打我呀 臭弟弟 我改你东西你有意见？？？");
 		   System.out.println("给你脸你还不要了 我今天不把你打的你妈都不认识我就不是你爸爸");
+System.out.println("给你脸你还不要了 我今天不把你打的你妈都不认识我就不是你爸爸");System.out.println("给你脸你还不要了 我今天不把你打的你妈都不认识我就不是你爸爸");System.out.println("给你脸你还不要了 我今天不把你打的你妈都不认识我就不是你爸爸");
 		
 	}
 
