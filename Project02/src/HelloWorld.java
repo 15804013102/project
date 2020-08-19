713ad990a658eb3a3894af17b4bc6168acd60011
15804013102 <1284648388@qq.com>
2020-08-19 16:28:18 +0800

    臭弟弟

--- Project02/src/HelloWorld.java
+++ Project02/src/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		   System.out.println("改你的东西 你打我呀");
 		   System.out.println("小逼崽子改我东西 你给我等着 看我打死你");
+		   System.out.println("给你脸你还不要了 我今天不把你打的你妈都不认识我就不是你爸爸");
 	}
 
 }
